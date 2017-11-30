d43318fae9e05cd85a1e3704f43309637ee63698
BilalMalik10 <bilalmmalik10@gmail.com>
2017-11-30 14:39:00 +0500

    update

---
+++ 1-RGB-GrayscaleImage/1-RGB-GrayscaleImage/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _1_RGB_GrayscaleImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.filterButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 368);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(413, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(568, 349);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(431, 367);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(61, 23);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // filterButton
+            // 
+            this.filterButton.Location = new System.Drawing.Point(498, 367);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(82, 23);
+            this.filterButton.TabIndex = 3;
+            this.filterButton.Text = "Apply Filter";
+            this.filterButton.UseVisualStyleBackColor = true;
+            this.filterButton.Click += new System.EventHandler(this.filterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(592, 399);
+            this.Controls.Add(this.filterButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RGB to Grayscale";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button filterButton;
+    }
+}
+
---
+++ 1-RGB-GrayscaleImage/1-RGB-GrayscaleImage/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_RGB_GrayscaleImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFile.FileName);
+                textBox1.Text = openFile.FileName.ToString();
+            }
+        }
+
+        private void filterButton_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            Bitmap grayScaleImg = new Bitmap(bmp.Width, bmp.Height);
+            int avg = 0;
+            for (int row = 0; row < bmp.Width; row++)
+            {
+                for (int col = 0; col < bmp.Height; col++)
+                {
+                    avg = 0;
+                    Color pixel = bmp.GetPixel(row,col);
+
+                    avg = (pixel.R + pixel.G + pixel.B) / 3;
+                    grayScaleImg.SetPixel(row, col, Color.FromArgb(avg, avg, avg));
+
+                }
+            }
+            pictureBox1.Image = grayScaleImg;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+    }
+}
